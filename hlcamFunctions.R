492840f162539def2dde1b5e57bc258e87513fcf
David Morrison <jdmorriso@gmail.com>
2017-08-10 17:13:43 +1000

    enh(reverse): completed reverse functions and added sq error tests

--- hlcamFunctions.R
+++ hlcamFunctions.R
@@ -7,11 +7,19 @@ MH <- matrix(c(0.38971, 0.68898, -0.07868, -0.22981, 1.18340, 0.04641, 0.0, 0.0,
 MCAT02INV <- solve(MCAT02)
 MCAT02INVY <- MCAT02INV[2,]
 MHMCAT02 <- MH %*% MCAT02INV
-Heh <- matrix(c(0.0, 100.0, 200.0, 300.0, 400.0, 0.8, 0.7, 1.0, 1.2, 0.8, 20.14, 90.0, 164.25, 237.53, 380.14), ncol=3)
+MHMCAT02INV <- solve(MHMCAT02)
 MSIG02 <- matrix(c(40.0/61.0, 20.0/61.0, 1.0/61.0, 1.0, -12.0/11.0, 1.0/11.0, 1.0/9.0, 1.0/9.0, -2.0/9.0), ncol=3, byrow=TRUE)
 MSIG02INV <- solve(MSIG02)
+Heh <- matrix(c(0.0, 100.0, 200.0, 300.0, 400.0, 0.8, 0.7, 1.0, 1.2, 0.8, 20.14, 90.0, 164.25, 237.53, 380.14), ncol=3)
 
 ## convert XYZ to CCT using McCamy's Approximation https://en.wikipedia.org/wiki/Color_temperature#Approximation
+
+# forward convert CCT to XYZ: CCT -> XYZ
+CCTtoXYZ <- function(CCT) {
+
+}
+
+# reverse convert XYZ to CCT: XYZ -> CCT
 XYZtoCCT <- function(Xw, Yw, Zw) {
   xw = Xw / (Xw + Yw + Zw)
   yw = Yw / (Xw + Yw + Zw)
@@ -20,23 +28,24 @@ XYZtoCCT <- function(Xw, Yw, Zw) {
   return (CCT)
 }
 
-## convert XYZ to LMS space using MCAT02 transform
-## returns an (n x 3) matrix
+
+## convert between XYZ and LMS spaces using the MCAT02 transform
+## returns an (n x 3) matrix, or just the Y component
+
+## forward convert XYZ to LMS space using MCAT02 transform: XYZ -> LMS
 XYZtoLMS <- function(X,Y,Z) {
   XYZ <- matrix(c(X,Y,Z), ncol=3)
   LMS <- t(MCAT02 %*% t(XYZ))
   return (LMS)
 }
 
-## convert LMS to XYZ space using inverse MCAT02 transform
-## returns an (n x 3) matrix
+## reverse convert LMS to XYZ space using inverse MCAT02 transform: LMS -> XYZ
 LMStoXYZ <- function(LMS) {
   XYZ <- t(MCAT02INV %*% t(LMS))
   return (XYZ)
 }
 
-## convert LMS to XYZ space using inverse MCAT02 transform
-## returns only the Y component
+## reverse convert LMS to XYZ space using inverse MCAT02 transform: LMS -> Y
 LMStoY <- function(LMS) {
   Y <- t(MCAT02INVY %*% t(LMS))
   return (Y)
@@ -58,6 +67,8 @@ calculateD <- function(A, La) {
 ## LMSs = LMS of the color sample to adapt
 ## LMSw = LMS of the white point (ie illuminant adopted white)
 ## LMSc = LMS of the adapted color sample (based on a reference white used in CIECAM02)
+
+# forward raw LMS to color adapted LMS: LMS, D, LMSw -> LMSc
 LMStoLMSc <- function(D, LMSs, LMSw) {
   Yw <- LMStoY(LMSw)
   Lc <- LMSs[,1] * (D * Yw / LMSw[,1] + 1 - D)
@@ -67,56 +78,76 @@ LMStoLMSc <- function(D, LMSs, LMSw) {
   return (LMSc)  
 }
 
+# reverse color adapted LMS to raw LMS: LMSc, D, LMSw -> LMS
+LMSctoLMS <- function(LMSc, D, LMSw) {
+  Yw <- LMStoY(LMSw)
+  L <- LMSc[,1] / (D * Yw / LMSw[,1] + 1 - D)  
+  M <- LMSc[,2] / (D * Yw / LMSw[,2] + 1 - D)  
+  S <- LMSc[,3] / (D * Yw / LMSw[,3] + 1 - D)  
+  LMS <- matrix(c(L, M, S), ncol=3)
+  return (LMS)  
+}
+
 ## perform post adaption conversion to cone responses using Hunt-Pointer-Esteves
 ## LMSc = LMS of the adapted color sample
 ## LMSp = Post Adaption Hunt-Point_estevez space
+
+# forward adapted LMS to Hunt-Point_estevez space: LMSc -> LMSp
 LMSctoLMSp <- function(LMSc) {
   LMSp = t(MHMCAT02 %*% t(LMSc))
   return (LMSp)
 }
 
-## convert to absolute cone responses
+# reverse Hunt-Point_estevez space to LMS adapted: LMSp -> LMSc
+LMSptoLMSc <- function(LMSp) {
+  LMSc = t(MHMCAT02INV %*% t(LMSp))
+  return (LMSc)
+}
+
+## convert to absolute cone responses based on physiological experiments by Valenton and Norren
+## they showed cone response of primates follows a sigmoidal curve influenced by level of adaption
 ## LMSp = Post Adaption Hunt-Point_estevez space
 ## LMSr = Absolute cone response
+
+# forward convert Post Adaption space to absolute cone response: LMSp, La -> LMSr
 LMSptoLMSr <- function(LMSp, La) {
   nc = 0.57
-  Lp <- LMSp[,1]^nc / (LMSp[,1]^nc + La^nc)
-  Mp <- LMSp[,2]^nc / (LMSp[,2]^nc + La^nc)
-  Sp <- LMSp[,3]^nc / (LMSp[,3]^nc + La^nc)
-  LMSr <- matrix(c(Lp, Mp, Sp), ncol=3)
+  Lr <- LMSp[,1]^nc / (LMSp[,1]^nc + La^nc)
+  Mr <- LMSp[,2]^nc / (LMSp[,2]^nc + La^nc)
+  Sr <- LMSp[,3]^nc / (LMSp[,3]^nc + La^nc)
+  LMSr <- matrix(c(Lr, Mr, Sr), ncol=3)
   return(LMSr)
 }
 
-## Calculate achromatic signal by averaging across cone responses in 40:20:1 ratio
-#LMSrtoA <- function(LMSr) {
-#  A <- (40 * LMSr[,1] + 20 * LMSr[,2] + LMSr[,3]) / 61
-#  return (A)
-#}
-
-
-## Calculate chromatic signals based on psychophysical results from Vos and Walraven
-#LMSrtoAB <- function(LMSr) {
-#  a <- 1/11 * (11 * LMSr[,1] - 12 * LMSr[,2] + LMSr[,3])
-#  b <- 1/9 * (LMSr[,1] + LMSr[,2] - 2 * LMSr[,3])
-#  AB  <- matrix(c(a, b), ncol=2)
-#  return (AB)
-#}
-
-LMSrtoA <- function(LMSr) {
-  A <- t(MSIG02[1,] %*% t(LMSr))
-  return (A)
+# reverse convert absolute cone response to Post Adaption space: LMSr, La -> LMSp
+LMSrtoLMSp <- function(LMSr, La) {
+  nc = 0.57
+  Lp <- (-La^nc * LMSr[,1] / (LMSr[,1] - 1))^(1/nc)
+  Mp <- (-La^nc * LMSr[,2] / (LMSr[,2] - 1))^(1/nc)
+  Sp <- (-La^nc * LMSr[,3] / (LMSr[,3] - 1))^(1/nc)
+  LMSp <- matrix(c(Lp, Mp, Sp), ncol=3)
+  return(LMSp)
 }
 
-LMSrtoAB <- function(LMSr) {
-  ab <- t(MSIG02[2-3,] %*% t(LMSr))
-  return (ab)
-}
+
 
 ## Calculate achromatic signal by averaging across cone responses in 40:20:1 ratio
 ## Calculate chromatic signals based on psychophysical results from Vos and Walraven
 ## Linear transform is stored in MSIG02
 
-# forward absolute cone response to chromatic/achromatic signals: LMSr -> Aab 
+# forward absolute cone response to achromatic signals: LMSr -> A 
+LMSrtoA <- function(LMSr) {
+  A <- t(MSIG02[1,] %*% t(LMSr))
+  return (A)
+}
+
+# forward absolute cone response to chromatic signals: LMSr -> ab 
+LMSrtoAB <- function(LMSr) {
+  ab <- t(MSIG02[2-3,] %*% t(LMSr))
+  return (ab)
+}
+
+# forward absolute cone response to both chromatic/achromatic signals: LMSr -> Aab 
 LMSrtoAab <- function(LMSr) {
   Aab <- t(MSIG02 %*% t(LMSr))
   return (Aab)
@@ -197,3 +228,70 @@ HueQtoh <- function(H) {
   h <- h - 360 * (h > 360)
   return (h)
 }
+
+# forward convert XYZ to JCH: XYZs, XYZw, La, A, E -> JHC
+XYZtoJHC <- function(Xs, Ys, Zs, Xw, Yw, Zw, La, Ambient, E) {
+  ## convert (color sample) and (adopted white) to LMS space
+  LMSs <- XYZtoLMS(Xs,Ys,Zs)
+  LMSw <- XYZtoLMS(Xw,Yw,Zw)
+  
+  ## use CAT02 to adapt the sample from the adopted white illumant to a reference white used in CIECAM02
+  D <- calculateD(Ambient, La)
+  LMSsc <- LMStoLMSc(D, LMSs, LMSw)
+  LMSwc <- LMStoLMSc(D, LMSw, LMSw)
+  
+  ## perform post adaption conversion to cone responses using Hunt-Pointer-Esteves
+  LMSsp <- LMSctoLMSp(LMSsc)
+  LMSwp <- LMSctoLMSp(LMSwc)
+  
+  ## convert to absolute cone responses
+  LMSsr <- LMSptoLMSr(LMSsp, La)
+  LMSwr <- LMSptoLMSr(LMSwp, La)
+  
+  ## Calculate achromatic and chromatic signals 
+  As <- LMSrtoA(LMSsr)
+  Aw <- LMSrtoA(LMSwr)
+  ab <- LMSrtoAB(LMSsr)
+  
+  ## calculate the perceived lightness (0 to 100), Hue Quadrature (0 to 400) and Chroma (0 to 100)
+  J <- AtoLightness(As, Aw, E)
+  H <- ABtoHueQ(ab)
+  C <- ABtoChroma(ab)
+
+  JHC <- matrix(c(J, H, C), ncol=3)
+  return(JHC)
+}
+
+# reverse convert JCH to XYZ: JHC, XYZw, La, A, E -> XYZs
+JHCtoXYZ <- function(Js, Hs, Cs, Xw, Yw, Zw, La, Ambient, E) {
+  ## calculate the achromatic white point for the target device
+  RLMSw <- XYZtoLMS(all$Xw,all$Yw,all$Zw)
+  RD <- calculateD(all$Ambient, all$La)
+  RLMSwc <- LMStoLMSc(RD, RLMSw, RLMSw)
+  RLMSwp <- LMSctoLMSp(RLMSwc)
+  RLMSwr <- LMSptoLMSr(RLMSwp, all$La)
+  RAw <- LMSrtoA(RLMSwr)
+  
+  # calculate the acrhomatic signal A from the sample lightness J
+  RAs <- LightnesstoA(all$Js1, RAw, 1.0)
+  
+  # calculate the chromatic ab signal from the Hue Quadrant and Chroma 
+  Rab <- HueQChromatoAB(all$Hs1, all$Cs1)
+  
+  # compute cone response LMS from the achromatic signal A and opponents a & b
+  RLMSsr <- AabtoLMSr(RAs, Rab)
+  
+  # compute cone signal LMS from the LMS cone response and La
+  RLMSsp <- LMSrtoLMSp(RLMSsr, all$La)
+  
+  # compute the color adapted LMS
+  RLMSsc <- LMSptoLMSc(RLMSsp)
+  
+  # compute the raw LMS
+  RLMS <- LMSctoLMS(RLMSsc, RD, RLMSw)
+  
+  # compute the XYZ
+  XYZ <- LMStoXYZ(RLMS)
+
+  return(XYZ)
+}  
